76e2217ba1c2971ff5f4e54e74131bd19364d222
Jose Quesada <jose.quesada@fernando3martos.com>
2024-08-07 09:03:37 +0200

    Mostrar consulta paises

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 import { AppComponent } from './app.component';
+import { provideHttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,10 +13,9 @@ import { AppComponent } from './app.component';
   imports: [
     AppRoutingModule,
     BrowserModule,
-    SharedModule,
-
+    SharedModule
   ],
-  providers: [],
+  providers: [provideHttpClient()],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/countries/countries-routing.module.ts
+++ src/app/countries/countries-routing.module.ts
@@ -23,6 +23,12 @@ const routes: Routes = [
     path: 'by/:id',
     component: CountryPageComponent,
   },
+  {
+    path: '**',
+    redirectTo: 'by-capital',
+  },
+
+
 ]
 
 @NgModule({
