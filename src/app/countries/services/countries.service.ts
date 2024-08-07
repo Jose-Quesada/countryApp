701b054a2fc0dc4b53fdaa92002addbaa9b39813
Jose Quesada <jose.quesada@fernando3martos.com>
2024-08-07 17:49:19 +0200

    Tarea país y región

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 
 import { Country } from '../interfaces/country';
 
@@ -13,8 +13,24 @@ export class CountriesService {
 
 
   searchCapital ( term: string): Observable<Country[]> {
-    return this.http.get<Country[]>(` ${ this.apiUrl }/capital/${ term }`);
+    return this.http.get<Country[]>(` ${ this.apiUrl }/capital/${ term }`)
+    .pipe (
+      catchError( () => of([]))
+    );
   }
 
+  searchCountry ( term: string): Observable<Country[]>{
+    return this.http.get<Country[]>(` ${ this.apiUrl }/name/${ term }`)
+    .pipe (
+      catchError( () => of([]))
+    );
+  }
+
+  searchRegion ( region: string): Observable<Country[]>{
+    return this.http.get<Country[]>(` ${ this.apiUrl }/region/${ region }`)
+    .pipe (
+      catchError( () => of([]))
+    );
+  }
 
 }
