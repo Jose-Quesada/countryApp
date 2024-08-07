76c135a5c67fabb8f94ff917aba23e67931bf1bd
Jose Quesada <jose.quesada@fernando3martos.com>
2024-08-07 20:30:20 +0200

    Argumentos por URL

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -11,6 +11,12 @@ export class CountriesService {
 
   constructor(private http: HttpClient) { }
 
+  searchContryByAplhaCode ( code : string): Observable<Country[]> {
+    return this.http.get<Country[]>(` ${ this.apiUrl }/alpha/${ code }`)
+    .pipe (
+      catchError( () => of([]))
+    );
+  }
 
   searchCapital ( term: string): Observable<Country[]> {
     return this.http.get<Country[]>(` ${ this.apiUrl }/capital/${ term }`)
