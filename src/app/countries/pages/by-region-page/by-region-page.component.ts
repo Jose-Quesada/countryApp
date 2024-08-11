c29cd07894bfb361801846e3225b5680d6c8e4ce
Jose Quesada <jose.quesada@fernando3martos.com>
2024-08-11 18:42:51 +0200

    Tarea mantener datos entre pantallas

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -1,21 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Country } from '../../interfaces/country';
+import { Region } from '../../interfaces/region.type';
 import { CountriesService } from '../../services/countries.service';
 
-type Region = 'Africa' | 'Americas' | 'Asia' | 'Europe' | 'Oceania';
+
 
 @Component({
   selector: 'app-by-region-page',
   templateUrl: './by-region-page.component.html',
   styles: ``
 })
-export class ByRegionPageComponent {
+export class ByRegionPageComponent implements OnInit {
   public countries: Country[] = [];
   public regions: Region[]= [ 'Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
   public selectedRegion?: Region;
 
   constructor ( private countriesService: CountriesService){}
+
+  ngOnInit(): void {
+    this.countries = this.countriesService.cacheStore.byRegion.countries;
+    this.selectedRegion = this.countriesService.cacheStore.byRegion.region;
+  }
 
   searchByRegion ( region : Region ):void{
 
--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,16 +1,23 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, of,map,delay } from 'rxjs';
+import { Observable, catchError, of,map, tap } from 'rxjs';
 
 import { Country } from '../interfaces/country';
+import { CacheStore } from '../interfaces/cache-store.interface';
+import { Region } from '../interfaces/region.type';
 
 @Injectable({providedIn: 'root'})
 export class CountriesService {
 
   private apiUrl: string = 'https://restcountries.com/v3.1'
 
+  public cacheStore: CacheStore= {
+    byCapital:    { term: '', countries: [] },
+    byCountries:  { term: '', countries: [] },
+    byRegion:     { region: '', countries: [] },
+  }
+
   constructor(private http: HttpClient) {
-    console.log('Inicialización de ContriesService');
    }
 
   private getCountriesRequest ( url: string ): Observable<(Country[])> {
@@ -32,21 +39,30 @@ export class CountriesService {
   searchCapital ( term: string): Observable<Country[]> {
 
     const url = `${ this.apiUrl }/capital/${ term }`;
-    console.log({ url });
-
-    return this.getCountriesRequest(url);
+    return this.getCountriesRequest(url)
+      .pipe(
+        tap( countries => this.cacheStore.byCapital = { term, countries})
+      );
   }
 
   searchCountry ( term: string): Observable<Country[]>{
 
     const url = ` ${ this.apiUrl }/name/${ term }`
-    return this.getCountriesRequest(url);
+    return this.getCountriesRequest(url)
+      .pipe(
+        tap( countries => this.cacheStore.byCountries = { term, countries})
+      );
+
   }
 
-  searchRegion ( region: string): Observable<Country[]>{
+  searchRegion ( region: Region): Observable<Country[]>{
 
     const url = ` ${ this.apiUrl }/region/${ region }`
-    return this.getCountriesRequest(url);
+    return this.getCountriesRequest(url)
+      .pipe(
+        tap( countries => this.cacheStore.byRegion = { region, countries})
+      );
+
   }
 
 }
